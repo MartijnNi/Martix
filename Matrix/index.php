8519111c2c66c6e8bce6a4bb845f40b820f6ab8a
Martijn <mhjnieuwenburg@gmail.com>
2023-05-16 15:02:21 +0200

    Falling code rain fixed
    to do:
    
    - fix screen size

---
+++ Matrix/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="navbar/navbar.css">
+    <link rel="stylesheet" href="/MatrixEfx/MatrixEfx.css">
+    <link rel="stylesheet" href="index.css">
+    <link href="https://fonts.googleapis.com/earlyaccess/sawarabigothic.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.6.0/p5.js" type="text/javascript"></script>
+    <title>Matrix | Home</title>
+</head>
+
+<body>
+    <?php include 'navbar/navbar.php'; ?>
+</body>
+<script src="/MatrixEfx/indexMatrix.js"></script>
+
+</html>
--- nav/nav.php
+++
@@ -1,7 +0,0 @@
-<nav>
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="about.php">About</a></li>
-        <li><a href="contact.php">Contact</a></li>
-    </ul>
-</nav>
